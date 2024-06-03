e2613e62da9accccc7a71a61d0925752a0981b4d
Кайдаров Павел <pavel.kaydarov@fabit.ru>
2024-06-03 10:51:12 +0300

    first commit

---
+++ game.js
@@ -0,0 +1,120 @@
+import {newGame} from "./index.js";
+
+export class Game {
+	members;
+	bot;
+	hours = [
+		1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+	];
+	votes = {};
+
+
+	constructor(bot, users) {
+		this.members = users;
+		this.bot = bot
+	}
+
+	sendMessage = async (msg, info, form) => {
+		await this.bot.sendMessage(msg.chat.id, info, {
+			...form,
+			message_thread_id: msg.message_thread_id
+		});
+	}
+
+	async restartGame (ctx, msg) {
+		this.votes = {}
+		await this.bot.editMessageText(this.getGameMessage(), {
+			...this.getButtons(),
+			chat_id: ctx.message.chat.id,
+			message_id: ctx.message.message_id,
+			message_thread_id: msg.message_thread_id
+		})
+	}
+
+	async initGame (msg) {
+		await this.sendMessage(msg, this.getGameMessage(), {
+			...this.getButtons(),
+			parse_mode: "HTML",
+		});
+
+		this.bot.on('callback_query', async ctx => {
+			try {
+				console.log(ctx)
+				if (ctx.data.startsWith('/restart')) {
+					if (Object.keys(this.votes).length) {
+						await this.restartGame(ctx, msg);
+					}
+				} else if (this.members.includes(`@${ctx.from.username}`)) {
+					if (!this.votes[`@${ctx.from.username}`]) {
+						this.votes[`@${ctx.from.username}`] = ctx.data;
+						await this.bot.editMessageText(this.getGameMessage(), {
+							...this.getButtons(),
+							chat_id: ctx.message.chat.id,
+							message_id: ctx.message.message_id,
+							message_thread_id: msg.message_thread_id
+						})
+					} else {
+						this.votes[`@${ctx.from.username}`] = ctx.data;
+					}
+				}
+			} catch (e) {
+				console.log(e)
+			}
+		});
+	}
+
+	getButtons () {
+		if (Object.keys(this.votes).length === this.members.length) {
+			return {
+				reply_markup: {
+					inline_keyboard: [
+						[{text: "Перезапуск", callback_data: `/restart` }]
+					]
+				}
+			}
+		}
+		return {
+			reply_markup: {
+				inline_keyboard: [
+					[...this.hours.slice(
+						0, Math.ceil(this.hours.length / 2)).map(
+						item => ({text: item, callback_data: item})
+					)
+					],
+					[...this.hours.slice(
+						Math.ceil(this.hours.length / 2), this.hours.length).map(
+						item => ({text: item, callback_data: item})
+					)
+					],
+					[{text: "Перезапуск", callback_data: `/restart` }]
+				]
+			},
+		}
+	}
+
+	getGameMessage () {
+		let message = 'Участники:\n\n'
+		if (this.votes === {}) {
+			this.members.forEach(item => {
+				message += `${item}\n\n`;
+			})
+		} else {
+			if (Object.keys(this.votes).length === this.members.length) {
+				this.members.forEach(item => {
+					message += `${item} - ${this.votes[item]}\n\n`;
+				})
+				message += `Среднее значение: ${Object.values(this.votes).reduce((acc, curr) => (acc + Number(curr)), 0)/Object.values(this.votes).length}`
+			} else {
+				this.members.forEach(item => {
+					if (this.votes[item]) {
+						message += `${item} - ♠\n\n`;
+					} else {
+						message += `${item}\n\n`;
+					}
+				})
+			}
+		}
+		return message;
+	}
+}
+
---
+++ index.js
@@ -0,0 +1,53 @@
+import TelegramBot from 'node-telegram-bot-api';
+import {Game} from "./game.js";
+
+const bot = new TelegramBot("6997825825:AAH2jZXXkUoeYYUHt-ZPw-XGLoQLWhdf2Wg", {
+	polling: {
+		interval: 300,
+		autoStart: true
+	}
+});
+
+const commands = [
+	{
+		command: "start",
+		description: "Запуск бота"
+	},
+]
+
+const sendMessage = async (tgbot, msg, info, form) => {
+	await tgbot.sendMessage(msg.chat.id, info, {
+		...form,
+		message_thread_id: msg.message_thread_id
+	});
+}
+
+export const newGame = async (msg) => {
+	const mentions =  msg.entities.filter(item => item.type === 'mention').map(item => msg.text.slice(item.offset, item.offset+item.length));
+	if (!mentions.length) {
+		await sendMessage(bot, msg, `Нужно упомянуть пользователей после команды!`);
+		return
+	}
+	const game = new Game(bot, mentions);
+	await game.initGame(msg);
+}
+
+bot.setMyCommands(commands);
+
+bot.on("polling_error", err => console.log(err.data?.error.message));
+
+bot.on('text', async (msg, asd) => {
+
+	try {
+		if(msg.text.startsWith('/start')) {
+			console.log(msg)
+			await newGame(msg)
+		}
+		else {
+			await bot.sendMessage(msg.chat.id, msg.text);
+		}
+	}
+	catch(error) {
+		console.log(error);
+	}
+})
